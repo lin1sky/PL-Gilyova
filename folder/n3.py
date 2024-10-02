f02524e1c8e2e55646d5ce28526e505d0b392a2d
lin1sky <139589193+lin1sky@users.noreply.github.com>
2024-10-02 09:14:33 +0300

    Add files via upload

---
+++ folder/n3.py
@@ -0,0 +1,10 @@
+n=int(input())
+if n<=1440:
+    hours=n%(60*24)//60
+    minutes=n%60
+    print(hours,':',minutes)
+else:
+    days=n//(60*24)
+    hours=n%(60*24)//60
+    minutes=n%60
+    print('День:', days, ', время:' hours,':',minutes)
